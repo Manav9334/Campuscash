f19ef1e154c43cac7015015c7714ff2320de589d
Manav Kumar <142525560+Manav9334@users.noreply.github.com>
2026-03-28 15:10:23 +0530

    updated api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: "https://campuscash-backend-04j9.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
