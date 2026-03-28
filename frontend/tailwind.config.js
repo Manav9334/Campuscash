386dec43dbb790d5ce712aaed1d72da94a55d2a2
Manav Kumar <142525560+Manav9334@users.noreply.github.com>
2026-03-28 15:50:13 +0530

    fix : tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",   // React files
+    "./public/index.html"           // HTML file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
