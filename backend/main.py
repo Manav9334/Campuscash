f11b9b759878ed7640d90754a36ab3d5312bdea1
Manav Kumar <142525560+Manav9334@users.noreply.github.com>
2026-03-28 22:34:52 +0530

    frontend domain added

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="CampusCash API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://campuscash-zeta.vercel.app/", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
